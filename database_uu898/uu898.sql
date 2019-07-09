5af2fa0476b05d39fa157d3bfe93cffcfa829600
Azrael <906347377@qq.com>
2019-07-09 20:46:02 +0800

    更新数据列表项添加唯一键

--- database_uu898/uu898.sql
+++ database_uu898/uu898.sql
@@ -16,12 +16,12 @@ create table uu_user_list(
 	sex int,
 	tel char(11),
 	email varchar(32),
-	ID_card bigint,
-	bank_card bigint,
+	ID_card bigint unique,
+	bank_card bigint unique,
 	bank varchar(32)
 );
 
-INSERT INTO uu_user_list VALUES(null,"tom",md5('123'),"1.jpg","tom",1,"13415614561","a1@163.com","110241199011011156","1235641","建行");
+INSERT INTO uu_user_list VALUES(null,"tom",md5('123'),"1.jpg","tom",1,"13415614561","a1@163.com","110241199011011106","1235601","建行");
 INSERT INTO uu_user_list VALUES(null,"jerry",md5('123'),"1.jpg","jerry",0,"13415614561","a1@163.com","110241199011011156","1235641","建行");
 
 #游戏类型
