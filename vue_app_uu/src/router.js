69a283bdefe8bee61a03acbc01dee4a20dc051bd
xubuyi <379811676@qq.com>
2019-07-12 20:44:43 +0800

    更新package包

--- vue_app_uu/src/router.js
+++ vue_app_uu/src/router.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import test from './views/test.vue'
 
 Vue.use(Router)
 
@@ -9,8 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component:test
     },
     {
       path: '/about',
