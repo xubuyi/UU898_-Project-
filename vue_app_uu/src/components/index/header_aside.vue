7fa92ed7077830ca8dda02e415320510220db514
tanbo <328117472@qq.com>
2019-07-16 11:56:45 +0800

    一楼组件及整合

--- vue_app_uu/src/components/index/header_aside.vue
+++ vue_app_uu/src/components/index/header_aside.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="lists" @mouseenter="enter" @mouseleave="leave">
       <!-- 游戏列表 -->
-      <ul>
+      <ul class="game">
         <li>
           <span class="my_img"></span>
           <span>热门游戏</span>
@@ -10,45 +10,53 @@
         <li v-for="(item,i) of gamne_list" :key="i">
           <span>{{item}}</span>
         </li>
-        <!-- <li>天涯明月刀</li>
-        <li>地下城与勇士</li>
-        <li>流放之路</li>
-        <li>诛仙3</li>
-        <li>诺亚传说</li>
-        <li>古剑奇谭网络版</li>
-        <li>灵山奇缘</li>
-        <li>传奇永恒</li>
-        <li>笑傲江湖OL</li> -->
         <li>更多游戏>></li>
       </ul>
       <div v-show="my_first" class="options">
-        <!-- 类型 -->
-        <ul class="my_type " >
-          <li @click="qq" v-for="(list,i) of game_type" :key="i">
-            <span>{{list}}</span>
-          </li>
-          <!-- <li>太阳账号</li>
-          <li>游戏币</li>
-          <li>装备</li>
-          <li>其他</li>
-          <li>游戏礼包</li>
-          <li>商城道具</li>
-          <li>点券</li>
-          <li>祝福酒</li> -->
-        </ul>
+        <div>
+          <!-- 类型 -->
+          <span class="aa">请选择商品类型</span>
+          <br />
+          <input type="text" placeholder="请选择商品类型" />
+          <br />
+          <ul class="games">
+            <li @click="area" v-for="(list,i) of game_type" :key="i">
+              <span>{{list}}</span>
+            </li>
+          </ul>
+        </div>
         <div v-show="c">
           <!-- 区服 -->
-          <ul>
-            <li>一区</li>
-            <li>二区</li>
-            <li>三区</li>
-            <li>四区</li>
-            <li>五区</li>
-            <li>六区</li>
-            <li>七区</li>
-            <li>八区</li>
-            <li>九区</li>
-            <li>十区</li>
+          <span>请选择游戏区</span>
+          <br />
+          <input type="text" placeholder="请选择游戏区" />
+          <ul class="my_type">
+            <li @click="next_area" v-for="(list,i) of game_area" :key="i">
+              <span>{{list}}</span></li>
+          </ul>
+        </div>
+        <div v-show="a">
+          <span>请选择游戏服务器</span>
+          <!-- 服务器 -->
+          <br />
+          <input type="text" placeholder="请选择游戏服务器" />
+          <ul class="my_type">
+            <li @click="buy" v-for="(list,i) of game_next_area" :key="i">
+              <span>{{list}}</span></li>
+          </ul>
+        </div>
+        <div v-show="s" class="buy">
+          <!-- 买卖按钮 -->
+          <ul class="my_buy">
+            <li>
+              <a href="javascript:;">购买商品</a>
+            </li>
+            <li>
+              <a href="javascript:;">发布商品</a>
+            </li>
+            <li>
+              <a href="javascript:;">发布担保</a>
+            </li>
           </ul>
         </div>
       </div>
@@ -62,25 +70,82 @@ export default {
     return {
       my_first: false,
       c: false,
-      gamne_list:["剑灵","天涯明月刀","地下城与勇士","流放之路","诛仙3","诺亚传说","  古剑奇谭网络版","灵山奇缘","传奇永恒","笑傲江湖OL"],
-      game_type:["游戏账号","太阳账号","游戏币","装备","其他","游戏礼包","商城道具","点券","祝福酒"],
+      a: false,
+      s: false,
+      gamne_list: [
+        "剑灵",
+        "天涯明月刀",
+        "地下城与勇士",
+        "流放之路",
+        "诛仙3",
+        "诺亚传说",
+        "  古剑奇谭网络版",
+        "灵山奇缘",
+        "传奇永恒",
+        "笑傲江湖OL"
+      ],
+      game_type: [
+        "游戏账号",
+        "太阳账号",
+        "游戏币",
+        "装备",
+        "其他",
+        "游戏礼包",
+        "商城道具",
+        "点券",
+        "祝福酒"
+      ],
+      game_area: [
+        "一区",
+        "二区",
+        "三区",
+        "四区",
+        "五区",
+        "六区",
+        "七区",
+        "八区",
+        "九区",
+        "十区",
+        "十一区",
+        "十二区"
+      ],
+      game_next_area: [
+        "子一区",
+        "子二区",
+        "子三区",
+        "子四区",
+        "子五区",
+        "子六区",
+        "子七区",
+        "子八区",
+        "子九区",
+        "子十区",
+        "子十一区",
+        "子十二区"
+      ]
     };
   },
   methods: {
     // 鼠标移入
-    enter() {
-      this.my_first = true;
-    },
-    //鼠标移出
-    leave() {
-      this.my_first = false;
-    },
-    //鼠标点击
-    qq() {
-      if (this.c == false) {
-        this.c = true;
+      enter() {
+        this.my_first = true;
+      },
+      //鼠标移出
+      leave() {
+        this.my_first = false;
+      },
+      //鼠标点击
+      area() {
+        if (this.c == false) {
+          this.c = true;
+        }
+      },
+      next_area(){
+        this.a = true
+      },
+      buy(){
+        this.s=true
       }
-    }
   }
 };
 </script>
@@ -95,18 +160,25 @@ ul {
   list-style: none;
 }
 // li样式
+ul.game > li {
+  border-bottom: 2px dotted #2c92e8;
+}
+//ul样式
 ul > li {
   width: 190px;
   height: 39px;
-  text-align: left;
-  line-height: 39px;
-  border-bottom: 2px dotted #2c92e8;
+  text-align: left;//文字左对齐
+  line-height: 39px;//文字居中
 }
 //背景色
-div {
+.game {
   background: #0086e6;
-  color: $color;
   font-size: $size;
+  color: $color;
+  margin: 0
+}
+div {
+  // background: #0086e6;
   display: inline-block;
 }
 //背景图
@@ -116,17 +188,95 @@ div {
   width: 25px;
   height: 29px;
 }
+//最外围相对定位
 .lists {
   position: relative;
 }
+//游戏信息绝对定位
 .options {
   position: absolute;
-  top: 0px;
-  left: 240px;
-  border: 1px solid red;
-  width:500px;
+  top: 0;
+  left: 230px;
+  width: 1200px;
+  height: 490px;
+  background: url("../../img/index/select-btn-bg.png");
+  color: #000;
+  z-index: 999
 }
-.my_type{
+//设置游戏信息以Y轴超出高度出现滑轮
+.my_type {
+  overflow-y: scroll;
+  overflow-x: auto;
+  height: 300px;
+  width: 210px;
+}
+//设置游戏信息以Y轴超出高度出现滑轮
+.games {
   display: inline-block;
+  overflow-y: scroll;
+  height: 300px;
+  width: 209px;
+}
+//设置每个li向上间距
+.buy > ul > li {
+  padding-top: 20px;
+}
+//li中的a
+.buy > ul > li > a {
+  display: inline-block;
+  width: 190px;
+  height: 41px;
+  text-decoration: none;
+  text-align: center;
+  color:#fff
+}
+//
+// .buy > ul > li > a:visited{
+//   color:transparent;
+// }
+//游戏买卖的按钮的背景
+.buy > ul > li:nth-child(1) > a {
+  background: url("../../img/index/select-btn.png") no-repeat;
+}
+//游戏买卖的按钮的背景
+.buy > ul > li:nth-child(2) > a {
+  background: url("../../img/index/select-btn.png") no-repeat 0px -40px;
+}
+//游戏买卖的按钮的背景
+.buy > ul > li:nth-child(3) > a {
+  background: url("../../img/index/select-btn.png") no-repeat 0px -81px;
+}
+//游戏信息ul上面的输入框样式
+input {
+  width: 209px;
+  height: 40px;
+  border: 1px solid #00b0ff;
+}
+//游戏信息每个ul给边框
+.options > div > ul {
+  border: 1px solid #00b0ff;
+  margin-top: 0;
+  width: 209px;
+  padding-left: 0;
+}
+//边距
+.options > div {
+  padding-left: 20px;
+  padding-top: 50px;
+}
+//游戏买卖的位置
+.options > .buy {
+  // display: inline-block;
+  float: right;
+  padding-top: 150px;
+  padding-right: 230px;
+}
+//间距
+.options>div>ul>li>span{
+  padding-left: 10px;
+}
+//取消游戏买卖的边框
+.my_buy{
+  border: none !important;
 }
 </style>
---
+++ vue_app_uu/src/components/index/index_right.vue
@@ -0,0 +1,291 @@
+<template>
+  <div>
+<div class="my_rights">
+      <div class="my_right">
+        <!-- 上面三个按钮 -->
+        <a href="javascript:;">
+          <i></i>
+          <span>我要买</span>
+        </a>
+        <a href="javascript:;">
+          <i></i>
+          <span>我要卖</span>
+        </a>
+        <a href="javascript:;">
+          <i></i>
+          <span>个人中心</span>
+        </a>
+      </div>
+      <div class="my_midd">
+        <!-- 中间的列表 -->
+        <ul>
+          <li>
+            <a href="javascript:;">公告</a>
+          </li>
+          <li>
+            <a href="javascript:;">帮助中心</a>
+          </li>
+          <li>
+            <a href="javascript:;">客服验证</a>
+          </li>
+        </ul>
+      </div>
+      <div class="my_msg">
+        <!-- 轮播的公告 -->
+        <ul>
+          <li>地下城与勇士收费公告</li>
+          <li>腾讯系列诚信保障账号恶意找回打击公告</li>
+        </ul>
+      </div>
+      <div class="my_ioc">
+        <!-- 下面的买东西按钮 -->
+        <ul>
+          <li>
+            <a href="javascript:;">
+              <em>9.98折</em>
+              <i></i>
+              <br>
+              <span>手机话费</span>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <i></i>
+              <br>
+              <span>QQ币</span>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <em>5折</em>
+              <i></i>
+              <br>
+              <span>视频会员</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="my_last">
+        <!-- 手游交易按钮 -->
+        <a href="javascript:;">手游交易</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+//最外围div宽
+.my_rights {
+  width: 250px;
+  // height: 480px;
+  margin-left: 10px;
+}
+// 上面三个按钮的样式
+.my_right > a {
+  display: inline-block;
+  width: 76px;
+  height: 76px;
+  text-decoration: none;
+  color: #fff;
+  text-align: center;
+  font: 14px "微软雅黑";
+  transition: all 0.3s linear;
+}
+//上面三个按钮的里面的图片样式
+.my_right > a > span {
+  width: 76px;
+  height: 21px;
+  display: block;
+}
+//动画效果
+.my_right > a > i {
+  transition: all 0.3s linear;
+}
+//动画效果
+.my_right > a > span {
+  transition: all 0.3s linear;
+}
+// 上面三个按钮的第一个按钮
+.my_right > a:first-child {
+  background: #fd5f57;
+}
+// 上面三个按钮的第一个按钮鼠标移入时的样式
+.my_right > a:first-child:hover {
+  background: #f0453c;
+}
+// 上面三个按钮的第一个按钮鼠标移入时的动画效果
+.my_right > a:first-child:hover > i {
+  transform: translateY(-3px);
+}
+// 上面三个按钮的第一个按钮鼠标移入时的动画效果
+.my_right > a:first-child:hover > span {
+  transform: translateY(-7px);
+}
+// 上面三个按钮的第二个按钮的样式
+.my_right > a:nth-child(2) {
+  background: #0aacf5;
+}
+// 上面三个按钮的第二个按钮鼠标移入时的样式
+.my_right > a:nth-child(2):hover {
+  background: #079fe4;
+}
+// 上面三个按钮的第二个按钮鼠标移入时的动画效果
+.my_right > a:nth-child(2):hover > i {
+  transform: translateY(-3px);
+}
+// 上面三个按钮的第二个按钮鼠标移入时的动画效果
+.my_right > a:nth-child(2):hover > span {
+  transform: translateY(-7px);
+}
+// 上面三个按钮的第三个按钮的样式
+.my_right > a:nth-child(3) {
+  background: #ff913d;
+}
+// 上面三个按钮的第三个按钮鼠标移入时的样式
+.my_right > a:nth-child(3):hover {
+  background: #eb7d29;
+}
+// 上面三个按钮的第三个按钮鼠标移入时的动画效果
+.my_right > a:nth-child(3):hover > i {
+  transform: translateY(-3px);
+}
+// 上面三个按钮的第三个按钮鼠标移入时的动画效果
+.my_right > a:nth-child(3):hover > span {
+  transform: translateY(-7px);
+}
+//上面三个按钮中的图片样式
+.my_right > a > i {
+  display: block;
+  width: 34px;
+  height: 34px;
+  margin: 10px auto 5px auto;
+}
+//上面三个按钮中的背景图片
+.my_right > a:first-child > i {
+  background: url("../../img/index/index_bg.png") no-repeat 3px -20px;
+}
+//上面三个按钮中的背景图片
+.my_right > a:nth-child(2) > i {
+  background: url("../../img/index/index_bg.png") no-repeat -35px -20px;
+}
+//上面三个按钮中的背景图片
+.my_right > a:nth-child(3) > i {
+  background: url("../../img/index/index_bg.png") no-repeat -66px -20px;
+}
+//上面三个按钮的左外边距
+.my_right > a + a {
+  margin-left: 10px;
+}
+//中间的公告样式
+.my_midd > ul {
+  list-style: none;
+  display: flex;
+  padding: 0;
+}
+//中间的公告样式
+.my_midd > ul > li a {
+  font: 13px/1.5 arial;
+  display: block;
+  width: 84px;
+  height: 40px;
+  text-align: center;
+  line-height: 38px;
+  text-decoration: none;
+  color: black;
+}
+//公告的蓝色上边框
+.my_midd > ul > li:first-child {
+  border-top: 1px solid #0aacf5;
+  font-weight: bold;
+}
+//取消轮播公告的点
+.my_msg >ul>li{
+  list-style: none;
+  
+}
+//下面买东西的按钮的样式
+.my_ioc > ul{
+  list-style: none;
+  display: flex;
+  padding: 0;
+  margin:0;
+}
+//下面a按钮的样式
+.my_ioc > ul>li>a{
+  display: block;
+  width:83px ;
+  height: 133px;
+  border:1px solid #e9e9e9;
+  color:#666;
+  text-decoration: none;
+}
+//a按钮里面的图片样式
+.my_ioc > ul>li>a>i{
+  display: inline-block;
+  width:36px;
+  height:36px;
+  text-align: center;
+  margin-left: 25px;
+  margin-top: 40px
+}
+//买东西的第一个a的样式
+.my_ioc > ul>li:first-child>a>i{
+  margin-left: 25px;
+  margin-top: 22px
+}
+//买东西的第三个a的样式
+.my_ioc > ul>li:last-child>a>i{
+  margin-left: 25px;
+  margin-top: 22px
+}
+//买东西的字体样式及位置
+.my_ioc > ul>li>a>span{
+  display: block;
+  font: 13px "微软雅黑";
+  text-align: center;
+  margin-top:20px 
+}
+//打折字样的样式
+em{
+  font-style: normal;
+  background: #C81522;
+  color:#fff;
+  font-size: 12px;
+  width:40px;
+  height:16px;
+  display: block;
+  text-align: center;
+}
+//买东西的a里面的背景图
+.my_ioc > ul>li:nth-child(1)>a >i{
+  background: url("../../img/index/rhg-ico.png") no-repeat 0px 0px;
+}
+//买东西的a里面的背景图
+.my_ioc > ul>li:nth-child(2)>a> i{
+  background: url("../../img/index/rhg-ico.png") no-repeat -40px 0px;
+}
+//买东西的a里面的背景图
+.my_ioc > ul>li:nth-child(3)>a >i{
+  background: url("../../img/index/rhg-ico.png") no-repeat -123px 0px;
+}
+//最后一个a的样式
+.my_last>a{
+  display: block;
+  background: #079fe4;
+  width: 83px;
+  height:133px;
+  text-align: center;
+  line-height: 133px;
+  text-decoration: none;
+  color:#fff;
+  font-size: 14px;
+  font-family: "微软雅黑";
+  margin-bottom: 20px;
+}
+</style>
