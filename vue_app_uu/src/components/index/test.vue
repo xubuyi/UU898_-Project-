069b7d34fbc851b4dc641e52eb6cf2231068863e
328117472 <328117472@qq.com>
2019-07-21 21:47:37 +0800

    vuex通过axios请求数据到全局

--- vue_app_uu/src/components/index/test.vue
+++ vue_app_uu/src/components/index/test.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
     测试
+    <div v-for="(list,i) of $store.state.list.list" :key="i">
+      游戏名称：{{list.game_name}}
+      ;
+      游戏公司：{{list.game_company_name}};
+      数量：{{list.game_turnover}};
+    </div>
+    <!-- {{$store.state.list.list}}
+    {{$store.state.list.type}}
+    {{$store.state.list.server}}-->
+    <!-- {{$store.state.list}}  -->
+    <!-- {{list}} -->
+    <!-- {{ $store.state.list.list}} -->
   </div>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        data:[]
+      }
+    },
+    created() {
+      this.$store.dispatch('getdata');
+      // this.list=$store.state.list
+      this.data=$store.state.list.list
+    },
+    // computed: {
+    //   data(){
+    //     return $store.state.list;
+    //   }
+    // },
   }
 </script>
 
--- vue_app_uu/src/store.js
+++ vue_app_uu/src/store.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    list:[]
   },
   mutations: {
-
+    getdata(state,res){
+      state.list=res.data;
+    }
   },
   actions: {
-
+    getdata(data){
+      axios.get("games").then(res=>{
+        data.commit("getdata",res)
+      })
+    }
   }
 })
