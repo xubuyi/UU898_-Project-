5a55eea1bcd73ae06ae851c88155ce5d4cdc3c74
tanbo <328117472@qq.com>
2019-07-22 19:13:01 +0800

    vuex全局数据例子

--- vue_app_uu/src/components/index/test.vue
+++ vue_app_uu/src/components/index/test.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     测试
-    <div v-for="(list,i) of $store.state.list.list" :key="i">
-      游戏名称：{{list.game_name}}
-      ;
+    <!-- <div v-for="(list,i) of $store.state.list.list" :key="i">
+      游戏名称：{{list.game_name}};
       游戏公司：{{list.game_company_name}};
       数量：{{list.game_turnover}};
-    </div>
+    </div> -->
     <!-- {{$store.state.list.list}}
     {{$store.state.list.type}}
     {{$store.state.list.server}}-->
     <!-- {{$store.state.list}}  -->
     <!-- {{list}} -->
     <!-- {{ $store.state.list.list}} -->
+    {{datas.list}}
   </div>
 </template>
 
@@ -20,13 +20,13 @@
   export default {
     data() {
       return {
-        data:[]
+        datas:{}
       }
     },
     created() {
       this.$store.dispatch('getdata');
       // this.list=$store.state.list
-      this.data=$store.state.list.list
+      this.datas=this.$store.state.list
     },
     // computed: {
     //   data(){
